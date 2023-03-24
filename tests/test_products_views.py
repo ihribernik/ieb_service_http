2526dca9d043ae5962711e0933e32dd74c0e90a3
Ivan Hribernik <cihribernik@gmail.com>
2023-03-24 18:44:10 -0300

    fix: tests

--- tests/test_products_views.py
+++ tests/test_products_views.py
@@ -1,8 +1,7 @@
-from django.test import SimpleTestCase
-from django.urls import reverse
-from django.contrib.auth.models import User
-from rest_framework.test import APIRequestFactory, force_authenticate
 from decouple import config
+from django.contrib.auth.models import User
+from django.test import SimpleTestCase
+from rest_framework.test import APIRequestFactory
 
 
 class TestProductsView(SimpleTestCase):
