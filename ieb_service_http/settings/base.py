af984ba451ba4f0ed05ac85d99089eb0d7541358
Ivan Hribernik <cihribernik@gmail.com>
2023-03-28 00:45:44 -0300

    fix: more cors

--- ieb_service_http/settings/base.py
+++ ieb_service_http/settings/base.py
@@ -51,20 +51,20 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_crontab",
-    "corsheaders",
     "ieb_service_http",
     "market",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "ieb_service_http.urls"
@@ -156,3 +156,4 @@ CRONJOBS = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
